e0238777c0d7146ad5c6bc7d742dc7c82816f741
amira <amira.a.a.hashim@gmail.com>
2020-07-14 23:07:32 +0200

    put header in seperate file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('adminlte.dashboard');
+        // return view('home');
+        return view("adminlte.dashboard");
     }
 }
