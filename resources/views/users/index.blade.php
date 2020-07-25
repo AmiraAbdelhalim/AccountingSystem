9cb7e5526726ed3ac25be0e6bc888181336318fe
amira <amira.a.a.hashim@gmail.com>
2020-07-25 17:27:07 +0200

    datatables

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,14 @@
+
+
 @extends('adminlte.dashboard')
 @section('title','Users')
 @section('section_title','Users')
 @section('content')
 <div class="container my-3">
-   
+    
     <br>
  
-    <table class="table">
+    <table class="table table-bordered data-table">
         <thead>
             <tr>
                 <th>ID</th>
@@ -27,4 +29,33 @@
         
     </table>
 </div>
+
+</body>
+@endsection
+@section('script')
+<script type="text/javascript">
+    $(function () {
+
+        var table = $('.data-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('users.index') }}",
+            columns: [
+                {
+                    data: 'id',
+                    name: 'id'
+                },
+                {
+                    data: 'name',
+                    name: 'name'
+                },
+                {
+                    data: 'email',
+                    name: 'email'
+                },
+                
+            ]
+        });
+        
+</script>
 @endsection
